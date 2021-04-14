bd5fd55cce29e6d776c1db517d149712366fca45
lisemadeline <lise.madeline19@gmail.com>
2021-04-14 18:39:11 +0200

    update logistic

--- logistic.py
+++ logistic.py
@@ -9,13 +9,12 @@ from fomlads.data.external import import_for_classification
 from fomlads.data.external import standard_scaler
 from fomlads.plot.evaluations import plot_roc
 from fomlads.plot.evaluations import plot_train_test_errors
-from fomlads.evaluate.eval_classification import confusion_matrix
+
 from fomlads.evaluate.eval_logistic import test_parameter_logistic
-from fomlads.evaluate.eval_classification import false_true_rates
-from fomlads.evaluate.eval_classification import score
+from fomlads.evaluate.eval_classification import score, two_class_cf_matrix, roc , roc_auc, two_class_f1_score
 
 from fomlads.model.classification import logistic_regression_fit
-from fomlads.model.classification import logistic_regression_predict
+from fomlads.model.classification import logistic_regression_predict, logistic_regression_prediction_probs
 from fomlads.model.classification import split_train_test
     
 
@@ -32,23 +31,39 @@ def fit_evaluate_logistic(train_inputs, train_targets, test_inputs, test_targets
     #fit the model to the train data
     weights = logistic_regression_fit(train_inputs, train_targets, threshold = 1e-8)
     #Get the prediction for the test data
-    predicts_test = logistic_regression_predict(test_inputs, weights)
- 
-    # Plot the corresponding ROC 
-    thresholds = np.linspace(0,1.5,100)
-    false_positive_rates, true_positive_rates = false_true_rates(test_inputs, test_targets, weights, thresholds)
-    fig1, ax1 = plot_roc(
-        false_positive_rates, true_positive_rates)
+    y_pred = logistic_regression_predict(test_inputs, weights)
+    #Metrics
+    print("Metrics:")
 
-    # Plot the confusion matrix
-    fig2, ax2 = confusion_matrix(test_targets, predicts_test)
+    #CF Matrix
+    print("Confusion Matrix")
+    cf = two_class_cf_matrix(test_targets, y_pred)
+    print(cf)
 
-    #Get evaluation scores
-    f1, precision, recall, accuracy = score(test_targets, predicts_test)
-    print(round(f1, 2), 'f1-score')
-    print(round(precision, 2), 'precision')
-    print(round(recall, 2), 'recall')
-    print(round(accuracy, 2), 'accuracy')
+    #F1 Score
+    print("F1_Score")
+    f1 = two_class_f1_score(test_targets, y_pred)
+    print(f1)
+
+
+
+    plt.figure(figsize=(15,7))
+    prob_vector = logistic_regression_prediction_probs(train_inputs, weights)
+    #print(prob_vector)
+    print(len(prob_vector))
+    ROC = roc(prob_vector,test_targets, partitions=100)
+    #print(ROC)
+    plt.scatter(ROC[:,0],ROC[:,1],color='#0F9D58',s=30)
+    plt.title('ROC Curve',fontsize=20)
+    plt.xlabel('False Positive Rate',fontsize=16)
+    plt.ylabel('True Positive Rate',fontsize=16)
+    fpr, tpr = ROC[:, 0], ROC[:, 1]
+    plt.show()
+
+    #AUC-ROC
+    auc = roc_auc(prob_vector,test_targets, partitions=100)
+    print('Area under curve={}'.format(auc))
+    plt.show()
 
 
     plt.show()
